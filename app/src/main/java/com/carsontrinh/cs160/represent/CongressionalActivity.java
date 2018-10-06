2477b3ec098b97ac2ed027f8ec486873c1778c34
Carson Trinh <carsontrinh@gmail.com>
2018-10-06 03:50:54 -0700

    Add Volley and API requests to Geocodia. Add more buttons.

--- app/src/main/java/com/carsontrinh/cs160/represent/CongressionalActivity.java
+++ app/src/main/java/com/carsontrinh/cs160/represent/CongressionalActivity.java
@@ -1,13 +1,102 @@
 package com.carsontrinh.cs160.represent;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class CongressionalActivity extends AppCompatActivity {
+
+    private final String API_KEY_GEOCODIO = "835eab88c6bb5cc7aed43f66aeebbb388bd25d8";
+
+    /** True if the provided input is a zipcode, and false if it is an address. */
+    private String locationType;
+    private String zipcode;
+    private String coordinates;
+
+    RequestQueue queue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_congressional);
+
+        initializeVolley();
+
+
+        Intent intent = getIntent();
+        locationType = intent.getStringExtra(MainActivity.LOCATION_TYPE_MESSAGE);
+
+        String address;
+
+        switch (locationType) {
+            case MainActivity.TYPE_ZIPCODE:
+                zipcode = intent.getStringExtra(MainActivity.ZIPCODE_MESSAGE);
+                address = zipcode;
+                break;
+            case MainActivity.TYPE_CURRENT_LOCATION:
+                coordinates = intent.getStringExtra(MainActivity.COORDINATES_MESSAGE);
+                address = coordinatesToAddress(coordinates);
+                break;
+            default:
+                return;
+        }
+
+        run(address);
+    }
+
+    /** Converts COORDINATES - a String "<latitude>,<longitude>" to an address using reverse-geocoding. */
+    private String coordinatesToAddress(String coordinates) {
+        // TODO
+        return null;
+    }
+
+    private void run(String address) {
+        final TextView volleyTextView = (TextView) findViewById(R.id.volleyText);
+
+        // Geocodio stuff.
+        StringBuilder query = new StringBuilder();
+        query.append("https://api.geocod.io/v1.3/");
+        query.append("geocode?");  // specifies if <geocode> or <reverse geocode> (for coords)
+        query.append("q="); query.append(address);  // address
+        query.append("&fields="); query.append("cd");  // congressional district
+        query.append("&api_key="); query.append(API_KEY_GEOCODIO);  // specifies API key
+
+        System.out.println("<100>: " + query.toString());
+
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, query.toString(), null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+                    JSONArray jsonArray = response.getJSONArray("results");
+                    
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+            }
+        });
+        queue.add(request);
+    }
+
+     /** Initializes a new Volley Request Queue. */
+    private void initializeVolley() {
+        queue = Volley.newRequestQueue(this);
     }
 }
--- app/src/main/java/com/carsontrinh/cs160/represent/MainActivity.java
+++ app/src/main/java/com/carsontrinh/cs160/represent/MainActivity.java
@@ -1,7 +1,11 @@
 package com.carsontrinh.cs160.represent;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.location.Location;
 import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
@@ -21,11 +25,24 @@ import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    public static final String LOCATION_TYPE_MESSAGE = "LOCATION_TYPE";
+    public static final String ZIPCODE_MESSAGE = "ZIPCODE";
+    public static final String COORDINATES_MESSAGE = "COORDINATES";
+
+    public static final String TYPE_ZIPCODE = "type-zipcode";
+    public static final String TYPE_CURRENT_LOCATION = "type-current-location";
+    public static final String TYPE_RANDOM_LOCATION = "type-random";
+
+
      // Code credit:
      // Location services: Used some code from
      // https://github.com/googlesamples/android-play-location/tree/master/BasicLocatorSample/java
 
     private static final String TAG = MainActivity.class.getSimpleName();
+
+    private static final int REQUEST_PERMISSIONS_REQUEST_CODE = 34;
+
 
     /**
      * Provides the entry point to the Fused Location Provider API.
@@ -37,9 +54,6 @@ public class MainActivity extends AppCompatActivity {
      */
     protected Location mLastLocation;
 
-    private String mLatitudeLabel;
-    private String mLongitudeLabel;
-
     private Double userLatitude;
     private Double userLongitude;
 
@@ -49,9 +63,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-        mLatitudeLabel = getResources().getString(R.string.latitude_label);
-        mLongitudeLabel = getResources().getString(R.string.longitude_label);
         mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
 
         findViewById(R.id.zipcode_submit_button).setEnabled(false);
@@ -62,6 +73,9 @@ public class MainActivity extends AppCompatActivity {
     private void run() {
         final EditText zipcodeEditText = (EditText) findViewById(R.id.zipcode_input);
         final Button zipcodeSubmitButton = (Button) findViewById(R.id.zipcode_submit_button);
+
+        final Button currentLocationButton = (Button) findViewById(R.id.current_location_button);
+        final Button randomLocationButton = (Button) findViewById(R.id.random_location_button);
 
         zipcodeEditText.addTextChangedListener(new TextWatcher() {
             @Override
@@ -88,8 +102,57 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 // TODO: Start intent to start congressional view
+                submitIntent(TYPE_ZIPCODE, zipcodeEditText.getText().toString());
             }
         });
+
+        currentLocationButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO: Start intent to start congressional view
+                System.out.println("<0>");
+                if (!checkPermissions()) {
+                    System.out.println("PERMISSIONS NOT ENABLED");
+                    requestPermissions();
+                } else {
+                    System.out.println("PERMISSIONS ENABLED");
+                    getLastLocation();
+                    TextView textView = (TextView) findViewById(R.id.textView2);
+                    textView.setText(getString(R.string.current_location, userLatitude, userLongitude));
+                    if (userLatitude != null && userLongitude != null) {
+                        submitIntent(TYPE_CURRENT_LOCATION, userLatitude.toString() + "," + userLongitude.toString());
+                    }
+                }
+            }
+        });
+
+        randomLocationButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO: Start intent to start congressional view
+            }
+        });
+
+
+    }
+
+    /** Submits an intent to switch to {@link CongressionalActivity}. */
+    private void submitIntent(String locationType, String input) {
+        Intent intent = new Intent(this, CongressionalActivity.class);
+        intent.putExtra(LOCATION_TYPE_MESSAGE, locationType);
+
+        System.out.println("<2>" + locationType);
+        switch (locationType) {
+            case TYPE_ZIPCODE:
+                intent.putExtra(ZIPCODE_MESSAGE, input);
+                break;
+            case TYPE_CURRENT_LOCATION:
+                intent.putExtra(COORDINATES_MESSAGE, input);
+                break;
+            default:
+                return;
+        }
+        startActivity(intent);
     }
 
     /** Returns whether or not a string is a valid 5-digit zipcode input. */
@@ -98,7 +161,7 @@ public class MainActivity extends AppCompatActivity {
             return false;
         }
         try {
-            int parsedZipcode = Integer.parseInt(zipcode);
+            Integer.parseInt(zipcode);
             return true;
         } catch (NumberFormatException e) {
             return false;
@@ -130,6 +193,29 @@ public class MainActivity extends AppCompatActivity {
                         }
                     }
                 });
+    }
+
+    /**
+     * Return the current state of the permissions needed.
+     */
+    private boolean checkPermissions() {
+        int permissionState = ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_COARSE_LOCATION);
+        return permissionState == PackageManager.PERMISSION_GRANTED;
+    }
+
+    private void requestPermissions() {
+        Log.i(TAG, "Requesting permission");
+        // Request permission. It's possible this can be auto answered if device policy
+        // sets the permission in a given state or the user denied the permission
+        // previously and checked "Never ask again".
+        startLocationPermissionRequest();
+    }
+
+    private void startLocationPermissionRequest() {
+        ActivityCompat.requestPermissions(MainActivity.this,
+                new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},
+                REQUEST_PERMISSIONS_REQUEST_CODE);
     }
 
 }
